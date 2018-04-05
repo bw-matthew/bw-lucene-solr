137e4788321aa0b9c0108b45e6693f04eb6b046e
Matthew Franglen <matthew@brandwatch.com>
2018-04-05 11:52:22 +0100

    Partial implementation of compressed output

---
+++ lucene/core/src/java/org/apache/lucene/store/CompressedIndexOutput.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.lucene.store;
+
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Extension of IndexOutput, operating over a compressed file.
+ * The length and position data is reported over the file as if it were uncompressed.
+ * The length of the original data is stored as a footer which is read as the index is created.
+ */
+public abstract class CompressedIndexOutput extends IndexOutput {
+
+  private final IndexOutput output;
+  private final OutputStream decompressed;
+
+  /** resourceDescription should be a non-null, opaque string
+   *  describing this resource; it's returned from
+   *  {@link #toString}. */
+  protected CompressedIndexOutput(IndexOutput output) {
+    super("CompressedIndexOutput(" + output.toString() + ")", output.getName());
+    this.output = output;
+
+    try {
+      this.decompressed = new GZIPOutputStream(new OutputStreamAdapter(output));
+    }
+    catch (RuntimeException e) {
+      throw e;
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void writeByte(byte b) throws IOException {
+    decompressed.write(b);
+  }
+
+  public void writeBytes(byte[] b, int offset, int length) throws IOException {
+    decompressed.write(b, offset, length);
+  }
+
+  public void close() throws IOException {
+    output.close();
+  }
+
+  public long getFilePointer() {
+
+  }
+
+  public long getChecksum() throws IOException {
+
+  }
+
+  private static class OutputStreamAdapter extends OutputStream {
+
+    private final IndexOutput output;
+
+    private OutputStreamAdapter(IndexOutput output) {
+      this.output = output;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+      output.writeByte((byte)(b & 0xFF));
+    }
+
+    @Override
+    public void write(byte b[], int off, int len) throws IOException {
+      output.writeBytes(b, off, len);
+    }
+
+  }
+
+}
